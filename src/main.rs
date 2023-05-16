c0795b7c79d69b9f1f003e42ef69fe6ccf0f5b73
Joseph Sanchez <josep@DESKTOP-EOTHT6C.localdomain>
2023-05-16 19:05:46 -0400

    working on if else statement interpreter

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use asalang::{program, Value, start_interpreter, Node};
 
 fn main() -> Result<(), nom::Err<(&'static str, nom::error::ErrorKind)>> {
   
-  let result = program(r#"if true {return 1;} else {return 2;}"#);
+  let result = program(r#"if 4>3 {return 1;} else if 3==3{return 3;} else {return 2;}"#);
   match result {
     Ok((unparsed,tree)) => {
       println!("Unparsed Text: {:?}", unparsed);
